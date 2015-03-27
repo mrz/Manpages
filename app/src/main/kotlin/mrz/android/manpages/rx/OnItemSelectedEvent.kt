b2cef74196bcc096649ac62a6401f8fa161e8fb8
Mattia Ziulu <mziulu@gmail.com>
2015-03-27 18:21:35 +0100

    Add OnItemSelectedEvent, OnSubscribeAdapterViewOnItemSelected

---
+++ app/src/main/kotlin/mrz/android/manpages/rx/OnItemSelectedEvent.kt
@@ -0,0 +1,7 @@
+package mrz.android.manpages.rx
+
+import android.view.View
+import android.widget.AdapterView
+
+class OnItemSelectedEvent(val parent: AdapterView<*>?, val view: View?, val position: Int?, val id: Long?)
+
